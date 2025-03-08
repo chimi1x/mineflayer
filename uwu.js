2598fca958d3941903fbde73cfe85d395a20ac32
chimi1x <mostefaxv@gmail.com>
2025-03-08 21:26:49 +0300

    Update and rename bot.js to uwu.js

--- uwu.js
+++ uwu.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 
 // Configuration
 const bot = mineflayer.createBot({
-  username: 'testbot', // Bot username
+  username: 'UwU', // Bot username
   host: 'chimi1x.aternos.me', // Server IP
   port: 42898, // Server port
   version: '1.16.1' // Minecraft version
@@ -29,18 +29,18 @@ bot.on('end', () => {
 // Sneak every 10 seconds
 setInterval(() => {
   bot.setControlState('sneak', true); // Start sneaking
-  console.log('Bot is sneaking...');
+  console.log('تربع');
 
   setTimeout(() => {
     bot.setControlState('sneak', false); // Stop sneaking after 1 second
-    console.log('Bot stopped sneaking.');
+    console.log('كعد يرتاح خطيه.');
   }, 1000); // Sneak for 1 second
 }, 10000); // Repeat every 10 seconds
 
 // Jump every 5 seconds
 setInterval(() => {
   bot.setControlState('jump', true); // Jump
-  console.log('Bot is jumping!');
+  console.log('البوت ديكمز بوياايييي!');
 
   setTimeout(() => {
     bot.setControlState('jump', false); // Stop jumping
